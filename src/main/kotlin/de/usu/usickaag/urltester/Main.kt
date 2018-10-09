b4ba6426d611216716abdcc1c6370c2081cf4f7e
Christopher Kaag <christopher.kaag@gmail.com>
2018-10-09 10:57:33 +0200

    streamlined output option

--- src/main/kotlin/de/usu/usickaag/urltester/Main.kt
+++ src/main/kotlin/de/usu/usickaag/urltester/Main.kt
@@ -1,6 +1,7 @@
 package de.usu.usickaag.urltester
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.MissingParameter
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
@@ -14,7 +15,7 @@ class TestCommand : CliktCommand() {
     val inputFile: File by option("-i","--input-file", help="Input file where every non-empty line is an url to test").file().required()
     val fromInclusive: Int? by option("-f","--from", help="Start index, inclusive (based on non-empty lines of inputFile)").int().restrictTo(0)
     val toExclusive: Int? by option("-t","--to", help="End index, exclusive (based on non-empty lines of inputFile)").int().restrictTo(0)
-    val outputFile: File? by option("-o","--output-file", help="Output file (where to write the CSV results)").file()
+    val outputFile: File by option("-o","--output-file", help="Output file (where to write the CSV results)").file().default(File("output.csv"))
     val prefixRemove: String by option("-r","--prefix-remove", help="Prefix to remove from each url (won't test any url that does not match that prefix)").default("")
     val prefixAdd: String by option("-a","--prefix-add", help="Prefix to add to each url after removing other prefix").default("")
 
@@ -23,9 +24,12 @@ class TestCommand : CliktCommand() {
         val inputFileContent: List<String> = inputFile.readLines(Charsets.UTF_8).filter { it.isNotBlank() }
         val fromInclusive: Int = this.fromInclusive?:0
         val toExclusive: Int = this.toExclusive?:inputFileContent.size
-        val outputFile: File = this.outputFile?:File("output.csv")
-        if (!outputFile.exists() || !outputFile.canWrite()) {
+        val outputFile: File = this.outputFile
+        if (!outputFile.exists()) {
             outputFile.createNewFile()
+        }
+        if (!outputFile.canWrite()) {
+            throw MissingParameter("Output file parameter '${outputFile.absolutePath}' was not a writeable file, aborting")
         }
         val existingOutputFileContent: List<String> = outputFile.readLines(Charsets.UTF_8).filter { it.isNotBlank() }
         val prefixToRemove: String = this.prefixRemove
